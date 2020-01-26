3efd128099179355469cd615aa5dce6ab35ef142
Michael Still <mikal@stillhq.com>
2020-01-26 16:59:20 +1100

    Run the daemon as a systemd unit.

--- shakenfist/daemon.py
+++ shakenfist/daemon.py
@@ -19,25 +19,11 @@ LOG = logging.getLogger(__file__)
 LOG.setLevel(logging.DEBUG)
 
 
-# Network mesh maintenance
-netmon_pid = os.fork()
-if netmon_pid == 0:
-    setproctitle.setproctitle('sf netmon')
-    node_ips = list(db.get_node_ips())
-    for n in db.get_networks():
-        network = net.Network(
-            uuid=n.uuid,
-            vxlan_id=n.vxid,
-            provide_dhcp=n.provide_dhcp,
-            physical_nic='eth0',
-            nodes=node_ips,
-            ipblock=n.netblock)
-        with util.RecordedOperation('network creation', network) as ro:
-            network.create()
-
-    while True:
-        time.sleep(30)
-
+def main():
+    # Network mesh maintenance
+    netmon_pid = os.fork()
+    if netmon_pid == 0:
+        setproctitle.setproctitle('sf netmon')
         node_ips = list(db.get_node_ips())
         for n in db.get_networks():
             network = net.Network(
@@ -47,30 +33,45 @@ if netmon_pid == 0:
                 physical_nic='eth0',
                 nodes=node_ips,
                 ipblock=n.netblock)
-            network.ensure_mesh(copy.deepcopy(node_ips))
+            with util.RecordedOperation('network creation', network) as ro:
+                network.create()
 
-setproctitle.setproctitle('sf main')
-LOG.info('netmod pid is %d' % netmon_pid)
+        while True:
+            time.sleep(30)
 
-while True:
-    time.sleep(10)
-    wpid, status = os.waitpid(-1, os.WNOHANG)
-    LOG.warning('Subprocess %d died' % wpid)
+            node_ips = list(db.get_node_ips())
+            for n in db.get_networks():
+                network = net.Network(
+                    uuid=n.uuid,
+                    vxlan_id=n.vxid,
+                    provide_dhcp=n.provide_dhcp,
+                    physical_nic='eth0',
+                    nodes=node_ips,
+                    ipblock=n.netblock)
+                network.ensure_mesh(copy.deepcopy(node_ips))
 
+    setproctitle.setproctitle('sf main')
+    LOG.info('netmod pid is %d' % netmon_pid)
 
-# instance = virt.Instance(
-#     uuid=str(uuid.uuid4()),
-#     name='foo',
-#     tenant=None,
-#     # image_url='https://cloud-images.ubuntu.com/disco/current/disco-server-cloudimg-amd64.img',
-#     image_url='http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img',
-#     root_size_gb=20,
-#     memory_kb=1 * 1024 * 1024,
-#     vcpus=1)
+    while True:
+        time.sleep(10)
+        wpid, status = os.waitpid(-1, os.WNOHANG)
+        if wpid != 0:
+            LOG.warning('Subprocess %d died' % wpid)
 
-# with util.RecordedOperation('allocate ip address', instance) as ro:
-#     network.allocate_ip_to_instance(instance)
-#     network.update_dhcp()
+    # instance = virt.Instance(
+    #     uuid=str(uuid.uuid4()),
+    #     name='foo',
+    #     tenant=None,
+    #     # image_url='https://cloud-images.ubuntu.com/disco/current/disco-server-cloudimg-amd64.img',
+    #     image_url='http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img',
+    #     root_size_gb=20,
+    #     memory_kb=1 * 1024 * 1024,
+    #     vcpus=1)
 
-# with util.RecordedOperation('instance creation', instance) as ro:
-#     instance.create()
+    # with util.RecordedOperation('allocate ip address', instance) as ro:
+    #     network.allocate_ip_to_instance(instance)
+    #     network.update_dhcp()
+
+    # with util.RecordedOperation('instance creation', instance) as ro:
+    #     instance.create()
