5146818da230c2d044a8bd899405eeb577b0d4b7
Michael Still <mikal@stillhq.com>
2020-06-10 16:42:57 +1000

    Move metrics to etcd.

--- shakenfist/db.py
+++ shakenfist/db.py
@@ -677,68 +677,24 @@ def get_events(object_type, object_uuid):
         pass
 
 
-class NodeMetric(Base):
-    __tablename__ = 'node_metrics'
-
-    fqdn = Column(String, primary_key=True)
-    metric = Column(String, primary_key=True)
-    value = Column(String)
-    timestamp = Column(DateTime)
-
-    def __init__(self, metric, value):
-        self.fqdn = config.parsed.get('NODE_NAME')
-        self.metric = metric
-        self.value = value
-        self.timestamp = datetime.datetime.now()
-
-    def export(self):
-        return {
-            'fqdn': self.fqdn,
-            'metric': self.metric,
-            'value': self.value,
-            'timestamp': self.timestamp
-        }
-
-
 def update_metric(metric, value):
-    ensure_valid_session()
-
-    try:
-        try:
-            metric = SESSION.query(NodeMetric).filter(
-                NodeMetric.fqdn == config.parsed.get('NODE_NAME')).filter(
-                    NodeMetric.metric == metric).one()
-            metric.value = value
-        except exc.NoResultFound:
-            metric = NodeMetric(metric, value)
-
-        SESSION.add(metric)
-    finally:
-        SESSION.commit()
+    node = config.parsed.get('NODE_NAME')
+    etcd.put('metric', node, metric,
+             {
+                 'fqdn': node,
+                 'metric': metric,
+                 'value': value,
+                 'timestamp': time.time()
+             })
 
 
 def get_metric(fqdn, metric):
-    ensure_valid_session()
-
-    try:
-        m = SESSION.query(NodeMetric).filter(
-            NodeMetric.fqdn == fqdn).filter(
-                NodeMetric.metric == metric).one()
-        return m.export()
-    except exc.NoResultFound:
-        pass
+    return etcd.get('metric', fqdn, metric)
 
 
 def get_metrics(fqdn):
-    ensure_valid_session()
-
-    try:
-        metrics = SESSION.query(NodeMetric).filter(
-            NodeMetric.fqdn == fqdn).all()
-        for m in metrics:
-            yield m.export()
-    except exc.NoResultFound:
-        pass
+    for m in etcd.get_all('metric', fqdn):
+        yield m
 
 
 def allocate_console_port(instance_uuid):
