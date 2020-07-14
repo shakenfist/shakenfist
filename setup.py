717a4d5f533f7816d31441978a43b4f0c6ea1401
Andy McCallum <andy@mandoon.com>
2020-07-14 15:17:23 +1000

    Allow pypi to see the README in markdown

--- setup.py
+++ setup.py
@@ -14,7 +14,16 @@
 # limitations under the License.
 
 import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setuptools.setup(
     setup_requires=['pbr'],
-    pbr=True)
+    pbr=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    )
