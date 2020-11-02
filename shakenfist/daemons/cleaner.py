47dfa4615d6b0cab97802251c25890cbb57a28f8
Andy McCallum <andy@mandoon.com>
2020-11-03 06:59:19 +1100

    Delay etcd compaction to reduce startup load

--- shakenfist/daemons/cleaner.py
+++ shakenfist/daemons/cleaner.py
@@ -1,6 +1,7 @@
 import etcd3
 import json
 import os
+import random
 import time
 
 from shakenfist.configuration import config
@@ -147,7 +148,9 @@ class Monitor(daemon.Daemon):
 
     def run(self):
         LOG.info('Starting')
-        last_compaction = 0
+
+        # Delay first compaction until system startup load has reduced
+        last_compaction = time.time() - random.randint(1, 20*60)
 
         while True:
             # Update power state of all instances on this hypervisor
