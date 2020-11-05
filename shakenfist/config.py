ae5a0dfc45b95545f4958ca9e894806b7d025b0f
Michael Still <mikal@stillhq.com>
2020-11-05 18:24:13 +1100

    Initial attempt at glusterfs support.

--- shakenfist/config.py
+++ shakenfist/config.py
@@ -113,6 +113,10 @@ class SFConfig(SFConfigBase):
     )
 
     # Other options
+    GLUSTER_ENABLED: bool = Field(
+        False,
+        description='If we are using gluster for shared block storage.',
+    )
     ZONE: str = Field(
         'shakenfist', description='What nova called an availability zone'
     )
