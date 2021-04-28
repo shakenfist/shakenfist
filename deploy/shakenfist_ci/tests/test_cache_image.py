a15b30d421d35ebd28436bb7a88ce32ae493e526
Michael Still <mikal@stillhq.com>
2021-04-28 16:11:33 +1000

    Use the right name of image_uuids in CI tests.

--- deploy/shakenfist_ci/tests/test_cache_image.py
+++ deploy/shakenfist_ci/tests/test_cache_image.py
@@ -14,14 +14,14 @@ class TestCacheImage(base.BaseNamespacedTestCase):
             '192.168.242.0/24', True, True, '%s-net' % self.namespace)
         self._await_network_ready(self.net['uuid'])
 
-    def test_cache_image(self):
+    def test_cache_image_specific(self):
         # It is currently not possible to check if an image is
         # in the cache via API, so for now we just cache this and
         # see if any errors come back.
         url = ('http://cloud.centos.org/centos/6/images/'
                'CentOS-6-x86_64-GenericCloud-1604.qcow2.xz')
-        self.system_client.cache_image(url)
-        self._await_image_download_success(url, after=time.time())
+        img = self.system_client.cache_image(url)
+        self._await_image_download_success(img['uuid'], after=time.time())
 
     def test_cache_invalid_image(self):
         url = ('https://nosuch.shakenfist.com/centos/6/images/'
