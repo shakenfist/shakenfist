e316eec9d8c4db85dbb323aa2f07dde3b7b0c8fa
Michael Still <mikal@stillhq.com>
2022-01-09 09:04:35 +1100

    Review comments.

--- shakenfist/daemons/queues.py
+++ shakenfist/daemons/queues.py
@@ -408,8 +408,12 @@ def instance_delete(inst):
                         n.delete_on_hypervisor()
 
 
-def snapshot(inst, disk, artifact_uuid, blob_uuid):
-    b = blob.snapshot_disk(disk, blob_uuid)
+def snapshot(inst, disk, artifact_uuid, blob_uuid, thin=False):
+    try:
+        b = blob.snapshot_disk(disk, blob_uuid, thin=thin)
+    except exceptions.BlobDependencyMissing:
+        pass
+
     a = Artifact.from_db(artifact_uuid)
 
     if b.state.value == blob.Blob.STATE_DELETED:
