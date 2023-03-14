84eae9158c47b4ecf628ced027c5b44747f79fe7
Michael Still <mikal@stillhq.com>
2023-03-14 17:31:51 +1100

    Add CI test for artifact upload.

---
+++ deploy/shakenfist_ci/tests/test_uploads.py
@@ -0,0 +1,24 @@
+import string
+
+from shakenfist_ci import base
+
+
+class TestUploads(base.BaseNamespacedTestCase):
+    def __init__(self, *args, **kwargs):
+        kwargs['namespace_prefix'] = 'uploads'
+        super(TestUploads, self).__init__(*args, **kwargs)
+
+    def test_upload(self):
+        upl = self.test_client.create_upload()
+        for _ in range(100):
+            self.test_client.send_upload(upl['uuid'], string.ascii_letters)
+
+        self.test_client.truncate_upload(upl['uuid'], 100)
+
+        for _ in range(50):
+            self.test_client.send_upload(upl['uuid'], string.ascii_letters)
+
+        a = self.test_client.upload_artifact('test', upl['uuid'])
+
+        self.assertEqual(
+            len(string.ascii_letters) * 50 + 100, a['blobs']['1']['size'])
