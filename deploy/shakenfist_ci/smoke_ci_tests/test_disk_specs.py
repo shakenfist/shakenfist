ca77815dc3694f8ca9d63fc682c9a98b12bff398
Michael Still <mikal@stillhq.com>
2024-12-12 05:48:50 +1100

    A leaner and faster CI for pre-merge testing.

---
+++ deploy/shakenfist_ci/smoke_ci_tests/test_disk_specs.py
@@ -0,0 +1,26 @@
+from shakenfist_ci import base
+
+
+class TestDiskSpecifications(base.BaseNamespacedTestCase):
+    def __init__(self, *args, **kwargs):
+        kwargs['namespace_prefix'] = 'diskspecs'
+        super().__init__(*args, **kwargs)
+
+    def test_default(self):
+        inst = self.test_client.create_instance(
+            'test-default-disk', 1, 1024, None,
+            [
+                {
+                    'size': 8,
+                    'base': base.CLUSTER_CI_IMAGE,
+                    'type': 'disk'
+                }
+            ], None, None)
+
+        self.assertIsNotNone(inst['uuid'])
+        self._await_instance_ready(inst['uuid'])
+
+        results = self._await_command(inst['uuid'], 'df -h')
+        self.assertEqual(0, results['return-code'])
+        self.assertEqual('', results['stderr'])
+        self.assertTrue('vda' in results['stdout'])
