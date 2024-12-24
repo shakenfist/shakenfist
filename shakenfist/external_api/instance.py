54e64f52b02540238c3a1344e90e3ab4bcecac89
Michael Still <mikal@stillhq.com>
2024-12-24 21:45:38 +1100

    More debugging information for nodes which are not ready.

--- shakenfist/external_api/instance.py
+++ shakenfist/external_api/instance.py
@@ -518,9 +518,13 @@ class InstancesEndpoint(sf_api.Resource):
         if placed_on:
             n = Node.from_db(placed_on, suppress_failure_audit=True)
             if not n:
-                return sf_api.error(404, 'Specified node does not exist')
-            if n.state.value != Node.STATE_CREATED:
-                return sf_api.error(404, 'Specified node not ready')
+                return sf_api.error(404, 'Specified node {placed_on} does not exist')
+            node_state = n.state.value
+            if node_state != Node.STATE_CREATED:
+                n.add_event(
+                    EVENT_TYPE_AUDIT, 'API query for node told node not ready',
+                    extra={'node_state': node_state})
+                return sf_api.error(404, f'Specified node {placed_on} not ready')
 
         # Make sure we've been given a valid configdrive option
         if not configdrive:
