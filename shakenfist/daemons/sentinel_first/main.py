cbd29f404fbf100d25d354a03ed6b5c00a52f66e
Michael Still <mikal@stillhq.com>
2024-12-29 08:06:24 +1100

    Reliable sentinel shutdown.

--- shakenfist/daemons/sentinel_first/main.py
+++ shakenfist/daemons/sentinel_first/main.py
@@ -1,5 +1,6 @@
 # NOTE(mikal): this daemon's role is to notice that the node has been started
 # or shutdown. You should never manually stop this daemon!
+import setproctitle
 import signal
 import threading
 import time
@@ -26,24 +27,24 @@ signal.signal(signal.SIGTERM, exit_gracefully)
 
 def main():
     global EXIT
-    last_checkin = 0
     LOG.info('Started')
+    setproctitle.setproctitle('sf-sentinel-first')
 
     n = Node.from_db(config.NODE_NAME)
     n.set_daemon_state('sentinel-first', Node.DAEMON_STATE_RUNNING)
     n.state = Node.STATE_DEGRADED
 
     while not EXIT.is_set():
-        if time.time() - last_checkin > 5:
-            Node.observe_this_node()
-            last_checkin = time.time()
-
-        time.sleep(0.5)
+        LOG.debug('Checking in')
+        Node.observe_this_node()
+        time.sleep(15)
 
     LOG.info('Stopping')
 
     # The default systemd timeout is 90 seconds, so wait just a bit less than
-    # that, although it shouldn't be needed.
+    # that, although it shouldn't be needed. This shouldn't really be needed
+    # as systemd should have done this already, but the logging might one day
+    # be helpful.
     start_time = time.time()
     all_daemons = n.get_registered_daemons()
     while all_daemons and time.time() - start_time < 80:
--- shakenfist/daemons/sentinel_last/main.py
+++ shakenfist/daemons/sentinel_last/main.py
@@ -1,6 +1,7 @@
 # NOTE(mikal): this daemon's role is to notice that you've exited the Shaken
 # Fist target run-level and therefore the node is stopping not going missing.
 # You should never manually stop this daemon!
+import setproctitle
 import signal
 import threading
 import time
@@ -27,18 +28,16 @@ signal.signal(signal.SIGTERM, exit_gracefully)
 
 def main():
     global EXIT
-    last_checkin = 0
     LOG.info('Started')
+    setproctitle.setproctitle('sf-sentinel-last')
 
     n = Node.from_db(config.NODE_NAME)
     n.set_daemon_state('sentinel-last', Node.DAEMON_STATE_RUNNING)
 
     while not EXIT.is_set():
-        if time.time() - last_checkin > 5:
-            Node.observe_this_node()
-            last_checkin = time.time()
-
-        time.sleep(0.5)
+        LOG.debug('Checking in')
+        Node.observe_this_node()
+        time.sleep(15)
 
     LOG.info('Stopping')
     n.set_daemon_state('sentinel-last', Node.DAEMON_STATE_STOPPED)
