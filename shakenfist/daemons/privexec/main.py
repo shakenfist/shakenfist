f402cc2029810815643cfa235ca74d747c73be88
Michael Still <mikal@stillhq.com>
2025-06-28 17:19:39 +1000

    It turns out the privexec event log was a bad idea.
    The database simply gets too big too quickly.

--- shakenfist/daemons/privexec/main.py
+++ shakenfist/daemons/privexec/main.py
@@ -12,14 +12,12 @@ import subprocess
 import threading
 import time
 
-from google.protobuf.json_format import MessageToJson
 from google.protobuf.message import DecodeError
 import psutil
 import setproctitle
 from shakenfist_utilities import random      # noreorder
 from shakenfist_utilities import logs        # noreorder
 
-from shakenfist.daemons.privexec import eventlog as privexec_eventlog
 from shakenfist.daemons.privexec import util as privexec_util
 from shakenfist.protos import common_pb2
 from shakenfist.protos import privexec_pb2
@@ -500,17 +498,7 @@ class PrivExecJob:
                 for request_field in request_map:
                     if request.HasField(request_field):
                         req = getattr(request, request_field)
-                        privexec_eventlog.EVENT_DB.write_event(
-                            'request', request_field, 'received request',
-                            extra=MessageToJson(req)
-                        )
-
                         reply = request_map[request_field](req)
-                        privexec_eventlog.EVENT_DB.write_event(
-                            'request', request_field, 'replied',
-                            extra=MessageToJson(reply)
-                        )
-
                         self.conn.sendall(reply.SerializeToString())
                         command_found = True
                         break
@@ -532,10 +520,6 @@ def write_pid_file():
 def main():
     write_pid_file()
     setproctitle.setproctitle('sf-privexec')
-
-    if os.path.exists(privexec_eventlog.DBPATH):
-        os.unlink(privexec_eventlog.DBPATH)
-    privexec_eventlog.EVENT_DB = privexec_eventlog.LocalEvents()
 
     if os.path.exists(SOCKET_PATH):
         os.unlink(SOCKET_PATH)
@@ -576,7 +560,6 @@ def main():
     LOG.info('Stopping')
 
     start_time = time.time()
-    last_event_prune = time.time()
     while workers:
         LOG.info(f'There are {len(workers)} remaining workers')
 
@@ -607,9 +590,6 @@ def main():
         workers = remaining_workers
         if workers:
             time.sleep(5)
-
-        if time.time() - last_event_prune > 300:
-            privexec_eventlog.EVENT_DB.prune_old_events()
 
     LOG.info(f'There are {len(workers)} remaining workers')
     LOG.info('Stopped')
--- shakenfist/daemons/privexec/util.py
+++ shakenfist/daemons/privexec/util.py
@@ -1,4 +1,3 @@
-from functools import partial
 import json
 import os
 import re
@@ -10,7 +9,6 @@ import time
 from shakenfist_utilities import logs                    # noreorder
 
 from shakenfist.config import config
-from shakenfist.daemons.privexec import eventlog as privexec_eventlog
 from shakenfist.exceptions import ListingInterfaceAddressesFailed
 
 
@@ -89,12 +87,8 @@ def _clean_ip_json(data):
 
 
 def check_for_interface(interface, namespace=None, up=False):
-    evt = partial(privexec_eventlog.EVENT_DB.write_event,
-                  'linux interface', interface)
-
     if namespace:
         if not os.path.exists('/var/run/netns/%s' % namespace):
-            evt(f'namespace {namespace} missing, interface missing')
             return False
 
         command = [locate_command('ip'), 'netns', 'exec', namespace]
@@ -105,25 +99,20 @@ def check_for_interface(interface, namespace=None, up=False):
         locate_command('ip'), '-pretty', '-json', 'link', 'show', interface
     ])
 
-    evt('executing command', extra=command)
     stdout, stderr, returncode = command_helper(
         *command, failure_is_error=False)
 
     if stderr.rstrip('\n').endswith(' does not exist.'):
-        evt('interface does not exist')
         return False
 
     if returncode != 0:
-        evt('unexpected error, interface missing')
         return False
 
     if up:
         j = _clean_ip_json(stdout)
         if 'UP' not in j[0]['flags']:
-            evt('interface exists, but is not up')
             return False
 
-    evt('interface exists')
     return True
 
 
@@ -133,13 +122,8 @@ def _get_safe_interface_name(interface):
 
 def create_interface(interface, interface_type, extra, mtu=None,
                      inner_namespace=None):
-    evt = partial(privexec_eventlog.EVENT_DB.write_event,
-                  'linux interface', interface)
-
     interface = _get_safe_interface_name(interface)
-    evt(f'safe interface name is {interface}')
     if check_for_interface(interface):
-        evt('skipping creation as it already exists')
         return True
 
     if not mtu:
@@ -156,15 +140,11 @@ def create_interface(interface, interface_type, extra, mtu=None,
     while True:
         last_attempt = attempts == 3
 
-        evt('executing command', extra=command)
         _, _, returncode = command_helper(
             *command, failure_is_error=last_attempt)
         if returncode == 0:
-            evt('interface creation success')
             break
-        evt('interface creation failure')
         if last_attempt:
-            evt('giving up after repeated failures')
             return False
 
         time.sleep(0.2)
@@ -175,23 +155,15 @@ def create_interface(interface, interface_type, extra, mtu=None,
             locate_command('ip'), 'link', 'set', f'{interface}-i',
             'netns', inner_namespace
         ]
-        evt('executing command', extra=command)
         _, _, returncode = command_helper(*command)
         if returncode != 0:
-            evt('failed to move interface to namespace')
             return False
-        evt('interface moved to namespace')
 
-    evt('interface created')
     return True
 
 
 def create_vx_interface(vx_interface, vx_id, vx_bridge, mesh_interface):
-    evt = partial(privexec_eventlog.EVENT_DB.write_event,
-                  'vxlan interface', vx_interface)
-
     if not check_for_interface(vx_interface):
-        evt('creating vxlan interface')
         create_interface(
             vx_interface, 'vxlan',
             ['id', str(vx_id), 'dev', str(mesh_interface), 'dstport', '0']
@@ -199,74 +171,54 @@ def create_vx_interface(vx_interface, vx_id, vx_bridge, mesh_interface):
 
         command = ['sysctl', '-w',
                    f'net.ipv4.conf.{vx_interface}.arp_notify=1']
-        evt('executing command', extra=command)
         _, _, returncode = command_helper(*command)
         if returncode != 0:
-            evt('failed to enable arp_notify for vxlan interface')
             return False
 
     if not check_for_interface(vx_bridge):
-        evt('creating vxlan bridge')
         create_interface(vx_bridge, 'bridge', [])
 
         command = [
             'ip', 'link', 'set', str(vx_interface), 'master', str(vx_bridge)
         ]
-        evt('executing command', extra=command)
         _, _, returncode = command_helper(*command)
         if returncode != 0:
-            evt('failed to set vxlan bridge as master for vxlan interface')
             return False
 
         command = ['ip', 'link', 'set', str(vx_interface), 'up']
-        evt('executing command', extra=command)
         _, _, returncode = command_helper(*command)
         if returncode != 0:
-            evt('failed to bring vxlan interface up')
             return False
 
         command = ['ip', 'link', 'set', str(vx_bridge), 'up']
-        evt('executing command', extra=command)
         _, _, returncode = command_helper(*command)
         if returncode != 0:
-            evt('failed to bring vxlan bridge up')
             return False
 
         command = ['sysctl', '-w', f'net.ipv4.conf.{vx_bridge}.arp_notify=1']
-        evt('executing command', extra=command)
         _, _, returncode = command_helper(*command)
         if returncode != 0:
-            evt('failed to enable arp_notify for vxlan bridge')
             return False
 
         command = ['brctl', 'setfd', str(vx_bridge), '0']
-        evt('executing command', extra=command)
         _, _, returncode = command_helper(*command)
         if returncode != 0:
-            evt('failed to set vxlan bridge forward delay')
             return False
 
         command = ['brctl', 'stp', str(vx_bridge), 'off']
-        evt('executing command', extra=command)
         _, _, returncode = command_helper(*command)
         if returncode != 0:
-            evt('failed to disable STP on vxlan bridge')
             return False
 
         command = ['brctl', 'setageing', str(vx_bridge), '0']
-        evt('executing command', extra=command)
         _, _, returncode = command_helper(*command)
         if returncode != 0:
-            evt('failed to set vxlan bridge aging')
             return False
 
     return True
 
 
 def get_interface_addresses(interface, namespace=None):
-    evt = partial(privexec_eventlog.EVENT_DB.write_event,
-                  'linux interface', interface)
-
     if namespace:
         command = [locate_command('ip'), 'netns', 'exec', namespace]
     else:
@@ -275,7 +227,6 @@ def get_interface_addresses(interface, namespace=None):
     command.extend([
         locate_command('ip'), '-pretty', '-json', 'addr', 'show', interface
     ])
-    evt('executing command', extra=command)
     stdout, stderr, returncode = command_helper(*command)
     if returncode not in [0, 1]:
         raise ListingInterfaceAddressesFailed(stderr)
@@ -284,15 +235,10 @@ def get_interface_addresses(interface, namespace=None):
     for elem in _clean_ip_json(stdout):
         for addr_info in elem.get('addr_info', []):
             addresses.append(addr_info['local'])
-
-    evt('interface addresses', extra=addresses)
     return addresses
 
 
 def add_address_to_interface(interface, namespace, address, netmask):
-    evt = partial(privexec_eventlog.EVENT_DB.write_event,
-                  'linux interface', interface)
-
     if namespace:
         command = [locate_command('ip'), 'netns', 'exec', namespace]
     else:
@@ -305,18 +251,13 @@ def add_address_to_interface(interface, namespace, address, netmask):
 
     attempts = 0
     while True:
-        evt('executing command', extra=command)
         _, stderr, returncode = command_helper(*command)
         if returncode == 0:
-            evt('added address success')
             return True
         if stderr.find('RTNETLINK answers: File exists') != -1:
-            evt('address already existed')
             return True
-        evt('add address failure')
 
         if attempts > 5:
-            evt('giving up after repeated failures')
             return False
 
         time.sleep(0.5)
@@ -324,19 +265,14 @@ def add_address_to_interface(interface, namespace, address, netmask):
 
 
 def create_network_namespace(namespace):
-    evt = partial(privexec_eventlog.EVENT_DB.write_event,
-                  'network namespace', namespace)
-
     if not os.path.exists('/var/run/netns/%s' % namespace):
         command = ['ip', 'netns', 'add', namespace]
-        evt('executing command', extra=command)
         _, stderr, returncode = command_helper(*command)
         if returncode != 0:
             r = re.compile(
                 r'Cannot create namespace file ".*": File exists\n')
             m = r.match(stderr)
             if not m:
-                evt('failed to set vxlan bridge forward delay')
                 return False
 
     return True
